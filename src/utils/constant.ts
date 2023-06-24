5b83a115ac78598f43ab0dc9ca0a260a33cddc52
anand <anand.prasad@appinventiv.com>
2023-06-24 12:14:26 +0530

    orm added

---
+++ src/utils/constant.ts
@@ -0,0 +1,5 @@
+const CONSTANT = {
+    connStr: `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`
+}
+
+export { CONSTANT };
