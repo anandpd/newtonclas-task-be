6ec4780cf6e15eb5626f7fc23c09e3e3404e5254
anand <anand.prasad@appinventiv.com>
2023-06-24 17:16:22 +0530

    mw added

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import cors from 'cors';
 import morganBody from 'morgan-body';
 import { CONSTANT } from './utils/constant';
 import { Ticket } from './models';
+import { errorHandler, notFoundHandler } from './utils/http-handlers';
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
@@ -13,6 +14,8 @@ app.use(cors());
 app.use(express.json());
 morganBody(app);
 
+app.get('/', async (req, res) => res.send("<p>Newoton BE</p>"))
+
 process.on('unhandledRejection', (e: Error) => {
     logger.error("unhandledRejection", e);
 });
@@ -20,11 +23,8 @@ process.on('uncaughtException', (e: Error) => {
     logger.error("uncaughtException", e);
 });
 
-app.get('/', async (req, res) => {
-    const response = await Ticket.findAll();
-    return res.json({
-        tickets: response
-    })
-})
+
+app.use(errorHandler);
+app.use(notFoundHandler);
 
 app.listen(PORT, () => logger.info(CONSTANT.LOGGING.SERVER.SUCCESS));
