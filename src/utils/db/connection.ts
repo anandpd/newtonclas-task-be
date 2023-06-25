b86480ee4506c671485788e1c85c2eccab41c2b0
anand <anand.prasad@appinventiv.com>
2023-06-25 21:34:46 +0530

    connection file handled

--- src/utils/db/connection.ts
+++ src/utils/db/connection.ts
@@ -4,10 +4,15 @@ import logger from '../logger';
 
 const sequelize = new Sequelize(CONSTANT.CONNECTION_STRING, { dialect: 'postgres', logging: false });
 (async () => {
-    await sequelize.authenticate();
-    logger.info(CONSTANT.LOGGING.DATABASE.SUCCESS);
-    await sequelize.sync({force: true});
-    logger.info(CONSTANT.LOGGING.DATABASE.SYNC);
+    try {
+        await sequelize.authenticate();
+        logger.info(CONSTANT.LOGGING.DATABASE.SUCCESS);
+        await sequelize.sync({});
+        logger.info(CONSTANT.LOGGING.DATABASE.SYNC);
+    } catch (error) {
+        logger.error(`Error in connecting DB ${error}`);
+        throw error;
+    }
 })()
 
 
